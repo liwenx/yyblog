717048416f466bf4281ab10f6f891a8afc166906
黎文兴 <liwxd@yonyou.com>
2018-01-17 14:52:02 +0800

    文件服务器

--- blog-web/src/main/java/com/yy/blog/BlogApplication.java
+++ blog-web/src/main/java/com/yy/blog/BlogApplication.java
@@ -1,12 +1,18 @@
 package com.yy.blog;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 /**
  * @author liwenxing
  * @date 2018/1/9 20:46
  */
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@Import(FdfsClientConfig.class)
 @SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
---
+++ blog-web/src/main/java/com/yy/blog/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.yy.blog.controller;
+
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * @author liwenxing
+ * @date 2018/1/16 10:29
+ */
+@RestController
+public class FileController {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+    /**
+     * 单个文件上传
+     * @author liwenxing
+     * @date 2018-1-16 10:39:20
+     * @param multipartFile
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/uploadFile")
+    public String uploadFile(MultipartFile multipartFile) throws IOException {
+        StorePath storePath = storageClient.uploadFile(multipartFile.getInputStream(), multipartFile.getSize(), FilenameUtils.getExtension(multipartFile.getOriginalFilename()), null);
+        return storePath.getFullPath();
+    }
+}
